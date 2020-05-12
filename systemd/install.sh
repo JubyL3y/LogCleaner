055f5434e8282732da31ff5c9973070e903b8487
Sam Smith <test@gmail.com>
2020-05-12 12:01:24 +0500

    Fix error in install.sh

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT="../log_clenaer.sh"
+SCRIPT="../log_cleaner.sh"
 SERVICE="log_cleaner.service"
 TIMER="log_cleaner.timer"
 INSTALL_PATH="/opt"
@@ -8,9 +8,10 @@ SERVICE_INSTALL_PATH="/etc/systemd/system"
 
 mkdir -p $INSTALL_PATH
 cp $SCRIPT $INSTALL_PATH
-chmod+X $INSTALL_PATH/log_cleaner.sh
+chmod +x $INSTALL_PATH/log_cleaner.sh
 
 cp $SERVICE $SERVICE_INSTALL_PATH
 cp $TIMER $SERVICE_INSTALL_PATH
 
 systemctl enable log_cleaner.timer
+systemctl start log_clenaer.timer
